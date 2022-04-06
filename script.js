2118f8c8d89d133e1fcfdd542fa4ebcf38f4a4ae
haroldmud <haroldmudosa@gmail.com>
2022-04-06 10:22:54 +0200

    Add correct path to anchor tags

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const hamburger = document.getElementById('link_menu');
 const navBar = document.getElementById('nav_bar');
 const title = document.getElementById('logo');
+const webLink = document.getElementById('portfolio');
+
 function changeIcon(icon) {
   icon.classList.toggle('fa-times');
 }
@@ -8,3 +10,7 @@ hamburger.addEventListener('click', () => {
   navBar.classList.toggle('show');
   title.style.opacity = '0';
 });
+
+function fun() {
+  location.reload();
+}
